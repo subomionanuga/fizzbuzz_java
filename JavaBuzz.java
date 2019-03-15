12721945113babd51e78148d5a72990a40b06664
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-03-15 16:38:41 +0000

    messing with strings

--- JavaBuzz.java
+++ JavaBuzz.java
@@ -1,5 +1,8 @@
 class JavaBuzz {
   public static void main(String[] args) {
+    String name = "Subomi";
+    int age = 25;
     System.out.println("First Java program");
+    System.out.println(name + " and I am " + age);
   }
 }
